a83bed66fd41a58ace83c33570b9b6c776b73b5f
Abris Simon <abris96@gmail.com>
2024-11-11 11:36:47 +0100

    lock fix

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -2,6 +2,6 @@ import puppeteer from "@cloudflare/puppeteer";
 import type { RequestHandler } from "./$types";
 
 export const GET = (async ({ platform }) => {
-  await puppeteer.launch(platform.env.BROWSER);
+  await puppeteer.launch(platform!.env.BROWSER);
   return new Response("", { status: 200 });
 }) satisfies RequestHandler;
